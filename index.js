4c2814aaad1041635bb8ec9ffcce96455c877f04
Dan Trofimov <44056222+DanTrofimov@users.noreply.github.com>
2022-11-13 13:30:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const { createProxyMiddleware } = require("http-proxy-middleware");
-require("dotenv").config();
-
-const PORT = process.env.PORT;
-const { API_BASE_URL } = process.env;
 
 const app = express();
 app.use(morgan("dev"));
 app.use(
 	"/",
 	createProxyMiddleware({
-		target: API_BASE_URL,
+		target: process.env.API_BASE_URL,
 		changeOrigin: true,
 	})
 );
 
-app.listen(PORT, () => {
-	console.log(`Starting Proxy at ${PORT}'s port`);
+app.listen(process.env.PORT, () => {
+	console.log(`Starting Proxy at ${process.env.PORT}'s port`);
 });
